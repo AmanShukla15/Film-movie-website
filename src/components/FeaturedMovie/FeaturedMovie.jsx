e45eca9e99fe59cd08b73d6b52119f4d1f26b3f1
Aman Shukla <94132741+AmanShukla15@users.noreply.github.com>
2024-01-02 22:17:35 +0530

    Movie website

---
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedMovie = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeaturedMovie
---
+++ src/services/TMDB.js
@@ -0,0 +1,46 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+
+
+export const tmdbApi = createApi({
+    reducerPath: 'tmdbApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+    endpoints: (builder) => ({
+
+        // Get genres
+        getGenres: builder.query({
+            query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+        }),
+
+        // Get movies by [type]
+        getMovies: builder.query({
+            query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+
+                // Get Movies by search
+                if (searchQuery) {
+                    return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`
+                }
+
+                //   getmovies by categories
+                if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string') {
+                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+                }
+
+                //   getmovies by genres
+                if (genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number') {
+                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+                }
+                
+                //   get popular movies
+                return `movie/popular?page=${page}&api_key=${tmdbApiKey}`
+            }
+        })
+    }),
+});
+
+export const {
+    useGetGenresQuery,
+    useGetMoviesQuery,
+
+} = tmdbApi;
